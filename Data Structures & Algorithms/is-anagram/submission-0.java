491802b20bc85aeed139cce503cf9b0e8e0e693b
Mayur Vitekar <vitekarmayur76@gmail.com>
2026-07-26 21:17:35 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,20 @@
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        HashMap<Character, Integer> freq = new HashMap<>();
+
+        for (char ch : s.toCharArray()) {
+            freq.put(ch, freq.getOrDefault(ch, 0) + 1);
+        }
+
+        for (char ch : t.toCharArray()) {
+            if (!freq.containsKey(ch)) return false;
+            freq.put(ch, freq.get(ch) - 1);
+            if (freq.get(ch) < 0) return false;
+        }
+
+        return true; // all counts balanced
+    }
+}
